0e5bd77191c06f78b370774703bf9dafa6465144
lavjul <43655372+lavjul@users.noreply.github.com>
2018-10-08 12:06:52 -0400

    Code_Complet V1

--- Projet_Robot/src/main/main.ino
+++ Projet_Robot/src/main/main.ino
@@ -26,7 +26,6 @@ const int ENCOCHES_TOTAL = 3200;
 const float DISTANCE_PAR_ENCOCHE = (DIAMETRE_ROUE * M_PI) / ENCOCHES_TOTAL;
 const float KP = 0.01 ;
 bool isForward = false;
-// FUCK !!!!!!!!!!!!
 
 
 /* ****************************************************************************
@@ -70,40 +69,13 @@ void TestTourner(bool turn)
 
 }
 
-//void PidLigneDroite
 
 /**
- * Methode qui accelere le robot
- * ACCEPTE SEULEMENT UN DESIRED SPEED DE 0 OU 0.5
+ * - Methode qui sert a calculer la distance en coche, call le PID et a reset les moteur a 0
  * 
- * 
-void Accel(float desiredSpeed, float currentSpeed)
-{
-    if (currentSpeed < desiredSpeed)
-    {
-      currentSpeed = currentSpeed + INCREMENTATION_ACCEL;
-      if (currentSpeed > desiredSpeed)
-        currentSpeed = desiredSpeed;
-    }
-    if (currentSpeed > desiredSpeed)
-    {
-      currentSpeed = currentSpeed - INCREMENTATION_ACCEL;
-      if (currentSpeed < desiredSpeed)
-        currentSpeed = desiredSpeed;
-    }
-    MOTOR_SetSpeed(0, currentSpeed);
-    MOTOR_SetSpeed(1, currentSpeed);
-
-  if(currentSpeed > 0 || currentSpeed < 0.5 )
-  {
-    delay(100);
-    Accel(desiredSpeed, currentSpeed);
-  }
-}
-*/
-
-
-
+ * - distanceEnCm s'explique de soi-meme, et desiredSpeed est la vitesse de 
+ *   croisiere puisque le robot accellere et descelere peu importe cette valeur
+ * */
 void Forward(float distanceEnCm, float desiredSpeed)
 {
   float distanceReelle = distanceEnCm/DISTANCE_PAR_ENCOCHE;
@@ -112,26 +84,42 @@ void Forward(float distanceEnCm, float desiredSpeed)
   MOTOR_SetSpeed(1, 0);
 }
 
+
+/**
+ * 
+ * - Methode qui fait le PID pour avancer en ligne droite, et qu'a la fois 
+ *   fait accellerer et descellerer le robot. De plus, le robot s'arrete apres 
+ *   d'avoir completer la distance desirer
+ * 
+ * - desiredSpeed est la vitesse de croisiere, et distanceEnCoche est la 
+ *   distance en nombres d'encoches a parcourir. 
+ * */
 void PidLigneDroite(float desiredSpeed, int distanceEnCoche)
 {
   float currentSpeed = 0;
   float smallDist = 40/DISTANCE_PAR_ENCOCHE;
   bool noAccel = false;
 
+  // Si petite distance, aucun point a accellerer jusqua une certaine vitesse
   if(distanceEnCoche<smallDist){
     noAccel = true;
     currentSpeed = 0.3;
   }
 
+  //L'incrementation descendant vite, avoir une valeur de 10 permet d'etre plus precis qu'une valeur de 0
   while(distanceEnCoche > 10)
   {
+    //code qui fait accellerer jusqua la vitesse de croisiere
     if (currentSpeed < desiredSpeed && !noAccel)
     {
       currentSpeed = currentSpeed + INCREMENTATION_ACCEL;
+      //du coups que lincrementation depasse la desiredSpeed
       if (currentSpeed > desiredSpeed)
         currentSpeed = desiredSpeed;
     }
 
+    //code qui fait descellerer
+    //jai hard code le 26, cest trash mais cest ce qui fittait le mieux pour un speed de 0.5
     if(distanceEnCoche < 26/DISTANCE_PAR_ENCOCHE)
     {
       desiredSpeed = 0;
@@ -144,8 +132,10 @@ void PidLigneDroite(float desiredSpeed, int distanceEnCoche)
     }
     MOTOR_SetSpeed(0, currentSpeed);
     MOTOR_SetSpeed(1, currentSpeed);
+    //code pour le PID
     ENCODER_Reset(0);
     ENCODER_Reset(1);
+    //meilleur valeur pour le PID selon hard test, MEN PARLER AVANT DE CHANGER
     delay(100);
     int master = ENCODER_Read(0);
     distanceEnCoche = distanceEnCoche + master;
@@ -157,13 +147,20 @@ void PidLigneDroite(float desiredSpeed, int distanceEnCoche)
 
 }
 
-
+/**
+ * - Methode qui permet de faire tourner le robot
+ * 
+ * - teta est langle a laquelle on veut tourner, et aGauche est true
+ *   si on veut tourner a gauche, sinon est false
+ * */
 void Turn (float teta, bool aGauche)
 {
-  float distToDo = M_PI * 39.15 * teta/360;
+  //je trouve la distance de l'arc en encoches 
+  float distToDo = (M_PI * 39.15 * teta/360)/DISTANCE_PAR_ENCOCHE;
   float speedTourner = 0.1;
   int moteur;
 
+  //Permet d'asigner le bon moteur pour tourner dans le bon sens selon le bool aGauche
   if(aGauche){
     moteur = 1;
   }
@@ -172,17 +169,17 @@ void Turn (float teta, bool aGauche)
     moteur = 0;
   }
 
+  //a chaque 25mili, je trouve le nombre d'encoches parcourue pi je le diminue a la distance a parcourir
   while(distToDo > 10)
   {
     ENCODER_Reset(moteur);
     MOTOR_SetSpeed(moteur, speedTourner);
     delay(25);
     float distTemp = ENCODER_Read(moteur);
-    Serial.println(distTemp);
     distToDo = distToDo - distTemp;
   }
+  //jarrete le moteur pour pas qui spin2win
   MOTOR_SetSpeed(moteur, 0);
-
 }
 
 /* ****************************************************************************
@@ -207,8 +204,19 @@ void loop() {
   //delay(10);// Delais pour décharger le CPU
   //TestEncodeurRoue();
   //TestTourner(0);
-  delay(10000);
+  delay(5000);
+  Forward(223, 0.5);
+  Turn(90, true);
+  Forward(50, 0.5);
+  Turn(90, false);
   Forward(50, 0.5);
   Turn(90, true);
+  Forward(25, 0.5);
+  Turn(45, false);
+  Forward(40, 0.5);
+  Turn(90, true);
+  Forward(70, 0.5);
+  Turn(90, false);
+  Forward(100, 0.5);
 
 }
